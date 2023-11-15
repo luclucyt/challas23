f45e3834bb5c27dd4330c2e50bd58fa15c91469c
Lucas van Briemen <vanbriemenlucas@gmail.com>
2023-11-15 14:16:57 +0100

    commit

--- view/login.view.php
+++ view/login.view.php
@@ -7,14 +7,14 @@
     <title>Document</title>
 
     <!-- Javascript -->
-    <script defer src="JS/login.js"></script>
+    <script defer src="js/login.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <!-- Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <?php
-    include ("../header.php");
+    include ("header.php");
 ?>
 
 <body>
