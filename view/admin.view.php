cd5ef5d77bc0110b5595c5e6cb8dbe1f0cc6d0fc
Lumenlum <115624862+Lumenlum@users.noreply.github.com>
2023-11-15 14:40:11 +0100

    admin meer

---
+++ view/admin.view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QR Code Scanner</title>
+    <link rel="stylesheet" href="admin.css">
+    <script src="JS/qrRead.js" type="module" defer></script>
+</head>
+
+<body>
+    <video id="preview"></video>
+    <div class="info">
+        <p id="foto"></p>
+        <p id="naam">Naam:</p>
+        <p id="email">E-mail:</p>
+        <p id="aanmeldingen">Aanmeldigen:</p>
+    </div>
+</body>
+
+</html>
