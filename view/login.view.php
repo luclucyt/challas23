56701bccaaa01f2f560ebeb186ef225c928f48b0
uhkz <115628083+uhkz@users.noreply.github.com>
2023-11-22 14:26:37 +0100

    login card kleiner gemaakt en altijd centered horizontal

--- view/login.view.php
+++ view/login.view.php
@@ -13,6 +13,7 @@
     <!-- Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
+        
         body {
             height: 100vh;
             /*background: linear-gradient(150deg, rgba(143, 81, 144, 1) 2%, rgba(167, 205, 72, 1) 29%, rgba(193, 65, 104, 1) 55%, rgba(188, 86, 55, 1) 61%, rgba(223, 199, 54, 1) 86%, rgba(81, 153, 191, 0.86) 100%);*/
@@ -20,6 +21,13 @@
             background-size: cover;
             background-position: 90% 0;
         }
+
+        .center-element {
+        margin: auto;
+        position: absolute;
+        top: 0; left: 0; bottom: 0; right: 0;
+        }
+        
 
         .kop {
             color: #A7CD48;
@@ -44,10 +52,10 @@
 include("header.php");
 ?>
 
-<section class="gradient-custom d-flex mt-3 align-items-center">
+<section class="center-element gradient-custom d-flex mt-5 align-items-center">
         <div class="container-fluid">
             <div class="row justify-content-center">
-                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div class="col-10 col-md-8 col-lg-6 col-xl-5">
                     <div class="card bg-dark h-100" style="border-radius: 1rem;">
                         <div class="card-body p-5 text-center d-flex flex-column justify-content-between h-100">
                             <div class="mb-md-5 mt-md-4 pb-2 login-wrapper">
