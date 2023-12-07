2c09e0da2f3297b37e321c88a0dd8f10dfe50eff
Lumenlum <115624862+Lumenlum@users.noreply.github.com>
2023-12-07 12:26:23 +0100

    phone 2

--- view/game1.view.php
+++ view/game1.view.php
@@ -5,15 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>space invader</title>
     
-
+    <!-- CSS -->
     <link rel="stylesheet" href="../css/game1.css">
 
+    <style>
+
+    </style>
 </head>
 <body>
     <div id="canvas-container">
         <canvas id="board"></canvas>
         
-
+        <!-- Buttons at the bottom -->
         <div id="buttons-container">
             <button id="leftButton">Left</button>
             <button id="rightButton">Right</button>
