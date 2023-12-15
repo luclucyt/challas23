1bebb2745cbc324a5c240af321ffabe3f94f131c
Lucas van Briemen <vanbriemenlucas@gmail.com>
2023-12-15 22:32:50 +0100

    commit

--- admin.php
+++ admin.php
@@ -1,6 +1,11 @@
 <?php
 include 'inc/_database.php';
 
+if(!isset($_SESSION['admin']) || $_SESSION['admin'] != 1){
+    header("Location: home.php");
+    exit();
+}
+
 
 include 'header.php';
 include 'view/admin.view.php';
